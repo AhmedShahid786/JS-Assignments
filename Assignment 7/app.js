7048e83f676b1a51fe270b0efa8c732c5b5450e3
Ahmed Shahid <a.razashahid19@gmail.com>
2024-04-29 00:18:03 +0500

    commit

--- Assignment 7/app.js
+++ Assignment 7/app.js
@@ -1,16 +1,3 @@
-// //? Question 1
-// let first_name = prompt("Enter Your First Name");
-// let last_name = prompt("Enter Your Last Name");
-// let full_name = first_name + last_name;
-// let result = document.getElementById("prop");
-// result.innerText = "Greetings " + full_name;
-
-// //? Question 2
-// let input = prompt("Enter Your Favourite Mobile Phone Model");
-// let result_q2 = input + " Length of string " + input.length;
-// let display = document.write(result_q2);
-// console.log(result_q2);
-
 let random_num = Math.ceil(Math.random() * 6);
 let user_num = document.getElementById("user_num");
 let actual_num = document.getElementById("actual_num");
